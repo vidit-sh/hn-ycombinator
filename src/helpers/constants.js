0031e62deb75aa3eb2695e97c24f5dbc71d77155
Your Name <viditisonline@gmail.com>
2019-04-02 15:35:16 +0530

    Upadted API Urls

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -5,6 +5,6 @@ export const ROUTES = {
 
 export const API_URLS = {
   [ROUTES.DEFAULT]:
-    'http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=30',
-  [ROUTES.NEWEST]: 'http://hn.algolia.com/api/v1/search_by_date?hitsPerPage=30'
+    'https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=30',
+  [ROUTES.NEWEST]: 'https://hn.algolia.com/api/v1/search_by_date?hitsPerPage=30'
 }
